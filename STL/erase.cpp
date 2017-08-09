1349d14ae65e5e638b0d35e407d06d98a3ebd5e7
ykmyou <ykmyou@naver.com>
2017-08-09 14:00:39 +0900

    STL

---
+++ STL/erase.cpp
@@ -0,0 +1,9 @@
+int erase(int* A, int s, int e){
+    int len = 0;
+    A[len++] = A[s];
+    for(int i = s + 1; i < e; i++){
+        if(A[len - 1] == A[i]) continue;
+        A[len++] = A[i]; 
+    }
+    return len;
+}
---
+++ STL/lower_bound.cpp
@@ -0,0 +1,8 @@
+int _lower_bound(int*A, int s, int e, int x){
+    while(s < e){  
+        int mid = (s + e) / 2;
+        if(A[mid] < x) s = mid + 1;
+        else e = mid;
+    }
+    return e + 1;
+}
