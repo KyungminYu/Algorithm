d50d07e14ff04aa271c722a5460244574562edd6
Ryu KyungMin <ykmyou@naver.com>
2017-10-02 13:13:29 +0900

    Divide & Conquer

---
+++ BOJ/2447 별찍기 - 10.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+char res[8000][8000];
+int dr[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int dc[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+void solve(int n, int r, int c){
+	if(n == 1) return;
+	if(n == 3){
+		for(int i = 0; i < 8; i++) res[r + dr[i]][c + dc[i]] = '*';
+		return;
+	}
+	int base = n / 3;
+	for(int i = 0; i < 8; i++) solve(base, r + dr[i] * base, c + dc[i] * base);
+}
+
+int main(){
+	int n; scanf("%d", &n);
+	for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) res[i][j] = ' ';
+	solve(n, n / 2, n / 2);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++)
+			printf("%c", res[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
