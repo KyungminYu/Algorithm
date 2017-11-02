08499b0b67ea53230683d1be28aa63673ad2c990
Ryu KyungMin <ykmyou@naver.com>
2017-11-02 10:24:12 +0900

    Update 14888 연산자 끼워넣기.cpp

--- BOJ/14888 연산자 끼워넣기.cpp
+++ BOJ/14888 연산자 끼워넣기.cpp
@@ -14,7 +14,7 @@ void solve(int depth, int val, int plus, int minus, int multi, int divide) {
 	if (divide > 0) solve(depth + 1, val / num[depth], plus, minus, multi, divide - 1);
 }
 int main() {
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) scanf("%d", &num[i]);
 	scanf("%d %d %d %d", &op[0], &op[1], &op[2], &op[3]);
