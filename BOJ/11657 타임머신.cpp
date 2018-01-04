c24ab9334e10209fb6a43253c516f3fc6751b77e
Ryu KyungMin <ykmyou@naver.com>
2018-01-04 11:58:48 +0900

    Bellman_Ford

--- BOJ/11657 타임머신.cpp
+++ BOJ/11657 타임머신.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <vector>
 using namespace std;
 const int NMAX = 501;
-const int INF = 1e9;
+const int INF = 0x3f3f3f3f;
 vector<pair<int, int> > G[NMAX];
 int dist[NMAX];
 int n, m;
@@ -13,10 +14,8 @@ int main(){
 		scanf("%d %d %d", &a, &b, &c);
 		G[a].push_back({b, c});
 	}
+	memset(dist, INF, sizeof(dist));
 	dist[1] = 0;
-	for(int i = 2; i <= n; i++)
-		dist[i] = INF;
-	int negativeCycle = 0;
 	for(int i = 1; i <= n; i++){
 		for(int j = 1; j <= n; j++){
 			for(int k = 0; k < G[j].size(); k++){
@@ -24,16 +23,15 @@ int main(){
 				int cst = G[j][k].second;
 				if(dist[j] != INF && dist[nxt] > dist[j] + cst){
 					dist[nxt] = dist[j] + cst;
-					if(i == n) negativeCycle = 1;
+					if(i == n) {
+						printf("-1");
+						return 0;
+					}
 				}
 			}
 		}
 	}
-	if(negativeCycle) printf("-1");
-	else{
-		for(int i = 2; i <= n; i++)
-			printf("%d\n", dist[i] == INF ? -1 : dist[i]);
-	}
-
+	for(int i = 2; i <= n; i++)
+		printf("%d\n", dist[i] == INF ? -1 : dist[i]);
 	return 0;
 }
