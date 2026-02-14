8d41819c81e6aeae34f1de2c41dbb85a851596eb
유경민 <ykmyou@gmail.com>
2026-02-14 21:01:42 +0900

    0001. update

--- LeetCode/0001.TwoSum/TwoSum.py
+++ LeetCode/0001.TwoSum/TwoSum.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        size = len(nums)
-        for i in range(0, size-1):
-            for j in range(i+1, size):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        cache = {}
+        for i, x in enumerate(nums):
+            require = target - x
+            if require in cache:
+                return [cache[require], i]
+            cache[x] = i
         return []
