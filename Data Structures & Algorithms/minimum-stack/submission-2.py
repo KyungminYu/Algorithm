0963f801e8acf41dd6283fcbaf6a0e5a9c1c6566
Yu Kyungmin <ykmyou@gmail.com>
2026-08-21 16:45:51 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.minStk = []
+        
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if len(self.minStk) == 0:
+            self.minStk.append(val)
+        else:
+            minVal = min(val, self.getMin())
+            self.minStk.append(minVal)
+
+    def pop(self) -> None:
+        self.stk.pop()
+        self.minStk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.minStk[-1]
+
+        
